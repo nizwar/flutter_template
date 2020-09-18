e497a795120f8eaa226e5709608f539e169ff3e9
Mochamad Nizwar Syafuan <nizwar@merahputih.id>
2020-09-18 13:55:16 +0700

    Okay, I forgot to write the Main!

--- main.dart
+++ main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'core/resources/myColors.dart';
 import 'ui/screens/mainScreen.dart';
 
-main() {}
+main() {
+  return runApp(
+    MultiProvider(
+      providers: [
+        //.Your Providers here.//
+        //.If you not using providers or just using one provider, just remove/replace the MultiProvider.//
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: primaryColor,
+          accentColor: accentColor,
+        ),
+        home: Root(),
+      ),
+    ),
+  ); 
+}
 
 class Root extends StatefulWidget {
   @override
@@ -18,14 +36,11 @@ class _RootState extends State<Root> {
   }
 }
 
-
 ///Splash screen to show on root!
 ///Use this if you got something to do and make some delay or loading when app is open
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
