04ade3f2801dd69af966f7634877ba04b1e53af5
Mochamad Nizwar Syafuan <nizwar_id@icloud.com>
2022-10-06 09:35:06 +0300

    Remove unused imports

--- core/utils/logger.dart
+++ core/utils/logger.dart
@@ -2,4 +2,4 @@ import 'dart:developer';
 import 'package:flutter/foundation.dart';
 
 void clog(dynamic object) => kDebugMode ? log(object.toString()) : null;
-void cprint(dynamic object) => kDebugMode ? print(object.toString()) : null;
+void cprint(dynamic object) => kDebugMode ? debugPrint(object.toString()) : null;
--- main.dart
+++ main.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
