cecc92c658dd8794eb2b7d47c1dc760a4ec63196
Mochamad Nizwar Syafuan <nizwar_id@icloud.com>
2022-10-12 15:05:35 +0300

    Add functions on httpconnection

--- core/https/http_connection.dart
+++ core/https/http_connection.dart
@@ -1,20 +1,36 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hayal/core/providers/globals/user_provider.dart';
 import '../models/model.dart';
 import '../resources/environment.dart';
 
 abstract class HttpConnection {
   final BuildContext context;
 
-  final Dio _dio = Dio(BaseOptions(baseUrl: endpoint));
+  Dio get _dio => Dio(
+        BaseOptions(
+          baseUrl: endpoint,
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer $apiToken}',
+          },
+        ),
+      );
 
   HttpConnection(this.context);
 
   ///if pure == true, it will return data without parse it to ApiResponse
-  Future get(String url, {Map<String, String>? params, dynamic headers, bool pure = false}) async {
+  Future get(String url, {Map<String, String>? params, dynamic headers, bool pure = true, bool showDebug = false}) async {
     try {
-      // _preRequest();
+      headers = _preRequest(headers);
+      _printRequest("get", "${_dio.options.baseUrl}$url${paramsToString(params)}", showDebug: showDebug);
       var resp = await _dio.get(url + paramsToString(params), options: Options(headers: headers));
+      _printResponse(resp.data, showDebug);
       if (pure) return resp.data;
       if (resp.data != null) {
         return ApiResponse.fromJson(resp.data);
@@ -25,10 +41,12 @@ abstract class HttpConnection {
   }
 
   ///if pure == true, it will return data without parse it to ApiResponse
-  Future post(String url, {Map<String, String>? params, dynamic body, dynamic headers, bool pure = false}) async {
+  Future post(String url, {Map<String, String>? params, dynamic body, dynamic headers, bool pure = true, bool showDebug = false}) async {
     try {
-      // _preRequest();
+      headers = _preRequest(headers);
+      _printRequest("post", "${_dio.options.baseUrl}$url${paramsToString(params)}", body: body, showDebug: showDebug);
       var resp = await _dio.post(url + paramsToString(params), data: body, options: Options(headers: headers));
+      _printResponse(resp.data, showDebug);
       if (pure) return resp.data;
       if (resp.data != null) {
         return ApiResponse.fromJson(resp.data);
@@ -39,10 +57,12 @@ abstract class HttpConnection {
   }
 
   ///if pure == true, it will return data without parse it to ApiResponse
-  Future put(String url, {Map<String, String>? params, dynamic body, dynamic headers, bool pure = false}) async {
+  Future put(String url, {Map<String, String>? params, dynamic body, dynamic headers, bool pure = true, bool showDebug = false}) async {
     try {
-      // _preRequest();
+      headers = _preRequest(headers);
+      _printRequest("put", "${_dio.options.baseUrl}$url${paramsToString(params)}", body: body, showDebug: showDebug);
       var resp = await _dio.put(url + paramsToString(params), data: body, options: Options(headers: headers));
+      _printResponse(resp.data, showDebug);
       if (pure) return resp.data;
       if (resp.data != null) {
         return ApiResponse.fromJson(resp.data);
@@ -53,10 +73,12 @@ abstract class HttpConnection {
   }
 
   ///if pure == true, it will return data without parse it to ApiResponse
-  Future delete(String url, {Map<String, String>? params, dynamic body, dynamic headers, bool pure = false}) async {
+  Future delete(String url, {Map<String, String>? params, dynamic body, dynamic headers, bool pure = true, bool showDebug = false}) async {
     try {
-      // _preRequest();
+      headers = _preRequest(headers);
+      _printRequest("delete", "${_dio.options.baseUrl}$url${paramsToString(params)}", body: body, showDebug: showDebug);
       var resp = await _dio.delete(url + paramsToString(params), data: body, options: Options(headers: headers));
+      _printResponse(resp.data, showDebug);
       if (pure) return resp.data;
       if (resp.data != null) {
         return ApiResponse.fromJson(resp.data);
@@ -70,16 +92,17 @@ abstract class HttpConnection {
   ///And got called before request called
   ///
   ///for an Example :
-  /// Map<String, String>? _preRequest(Map<String, String>? headers) {
-  ///   var userProvider = UserProvider.of(context);
-  ///   if (userProvider.authToken != null) {
-  ///     if (headers != null)
-  ///       headers.addEntries([MapEntry("Access-Token", "Bearer ${userProvider.authToken!.accessToken}")]);
-  ///     else
-  ///       headers = {"Access-Token": "Bearer ${userProvider.authToken!.accessToken}"};
-  ///   }
-  ///   return headers;
-  /// }
+  Map<String, String>? _preRequest(Map<String, String>? headers) {
+    var userProvider = UserProvider.of(context);
+    if (userProvider.token != null) {
+      if (headers != null) {
+        headers.addEntries([MapEntry("Access-Token", "Bearer ${userProvider.token!.accessToken}")]);
+      } else {
+        headers = {"Access-Token": "Bearer ${userProvider.token!.accessToken}"};
+      }
+    }
+    return headers;
+  }
 
   static String paramsToString(Map<String, String>? params) {
     if (params == null) return "";
@@ -88,6 +111,40 @@ abstract class HttpConnection {
       output += "$key=$value&";
     });
     return output.substring(0, output.length - 1);
+  }
+
+  ///Print request to console for debug purpose
+  void _printRequest(String type, String url, {Map<String, dynamic>? body, bool showDebug = false}) {
+    if (kDebugMode && showDebug) {
+      log("${type.toUpperCase()} REQUEST : $url \n");
+      if (body != null) {
+        try {
+          JsonEncoder encoder = const JsonEncoder.withIndent('  ');
+          String prettyprint = encoder.convert(body);
+          log("BODY : $prettyprint");
+        } catch (e) {
+          log("CAN'T FETCH BODY");
+        }
+      }
+      log("==================== WAIT FOR RESPONSE =========================\n\n");
+    }
+  }
+
+  ///Print response to console for debug purpose
+  void _printResponse(dynamic response, [bool showDebug = false]) {
+    String? prettyprint;
+    log(response.toString());
+    if (response is Map) {
+      try {
+        JsonEncoder encoder = const JsonEncoder.withIndent('  ');
+        prettyprint = encoder.convert(response);
+        // ignore: empty_catches
+      } catch (e) {}
+    }
+    if (kDebugMode && showDebug) {
+      log(prettyprint ?? response.toString());
+      log("=======================================================\n\n");
+    }
   }
 }
 
