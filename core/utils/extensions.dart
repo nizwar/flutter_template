b32111bc20ecebbad323592921c5eba7542cacf6
Mochamad Nizwar Syafuan <nizwar_id@icloud.com>
2023-10-31 16:14:34 +0700

    Add showProgress to shows adaptiveprogressindicator

--- core/utils/extensions.dart
+++ core/utils/extensions.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:igenyes_ferfi_app/ui/components/adaptive_progress_indicator.dart';
+import 'package:ndialog/ndialog.dart';
 
 extension KeyboardDismisser on Widget {
   Widget dismissKeyboardOnTap(BuildContext context) {
@@ -7,4 +9,8 @@ extension KeyboardDismisser on Widget {
       child: this,
     );
   }
+}
+
+extension FutureProgressDialog<T> on Future<T> {
+  Future<T?> showProgress(BuildContext context) => showCustomProgressDialog(context, loadingWidget: const AdaptiveProgressIndicator());
 }
