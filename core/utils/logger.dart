545f150050f7bd3dd64e6040578a8273adc9fc59
Mochamad Nizwar Syafuan <nizwar_id@icloud.com>
2024-11-18 13:00:09 +0700

    Add elog for Error Log (automate to crashlytics once it on !kDebugMode

--- core/utils/logger.dart
+++ core/utils/logger.dart
@@ -1,5 +1,26 @@
+import 'dart:convert';
 import 'dart:developer';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 
-void clog(dynamic object) => kDebugMode ? log(object.toString()) : null;
+void clog(dynamic object) {
+  if (kDebugMode) {
+    if (object is Map || object is List) {
+      try {
+        log(const JsonEncoder.withIndent("    ").convert(object));
+        return;
+      } catch (_) {}
+    }
+    log(object.toString());
+  }
+}
+
+void elog(dynamic object) {
+  if (kDebugMode) {
+    clog(object);
+  } else {
+    FirebaseCrashlytics.instance.log(object);
+  }
+}
+
 void cprint(dynamic object) => kDebugMode ? debugPrint(object.toString()) : null;
