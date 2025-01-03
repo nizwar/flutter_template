04b80613e0d01c399fcdb88aa2ea553fcc6033d1
Mochamad Nizwar Syafuan <nizwar_id@icloud.com>
2025-01-03 14:52:55 +0700

    Add equatable and print functions on model

--- core/models/model.dart
+++ core/models/model.dart
@@ -1,6 +1,18 @@
-abstract class Model {
+import 'package:equatable/equatable.dart';
+
+import '../utils/logger.dart';
+
+abstract class Model extends Equatable {
   Map<String, dynamic> toJson();
 
   @override
   String toString() => toJson().toString();
+
+  @override
+  List<Object?> get props => [toString()];
+
+  @override
+  bool? get stringify => true;
+
+  void print() => clog(toJson());
 }
