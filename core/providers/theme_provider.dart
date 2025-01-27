e48bf5d2afea99590031754b209cdc6482b45548
Mochamad Nizwar Syafuan <nizwar_id@icloud.com>
2025-01-27 19:00:12 +0700

    Set theme color on theme provider declaration

--- core/providers/theme_provider.dart
+++ core/providers/theme_provider.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import '../resources/colors.dart';
 import '../utils/utils.dart';
 
 class ThemeProvider extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.system;
   ThemeMode get themeMode => _themeMode;
 
-  ///TODO: Change color here
-  MaterialColor get colorSwatch => getMaterialColor(primaryColor);
+  final Color _colorSwatch;
 
+  ThemeProvider(this._colorSwatch);
+
+  MaterialColor get colorSwatch => getMaterialColor(_colorSwatch);
+
+  ///Check if the current theme is dark mode
+  ///
+  ///Instead of ThemeMode.dark we use the brightness of the theme instead, because when the theme mode is system
+  ///and the brightness can be dark.
   bool isDarkMode(BuildContext context) => theme(context).brightness == Brightness.dark;
 
+  ///Get the current theme
   void setThemeMode(ThemeMode themeMode) {
     _themeMode = themeMode;
     notifyListeners();
