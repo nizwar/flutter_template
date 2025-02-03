a4695f42fb2d8ba8b04f111c841acdb9d07493e0
Mochamad Nizwar Syafuan <nizwar_id@icloud.com>
2025-02-03 15:35:58 +0700

    Environment of appconfigs

--- core/resources/environment.dart
+++ core/resources/environment.dart
@@ -1,1 +1,14 @@
-const String endpoint = "";
+import 'package:boilerplate/core/utils/app_config.dart';
+import 'package:flutter/material.dart';
+
+/// Development mode configuration.
+class DevelopmentMode extends AppConfig {
+  /// Creates a new instance of [DevelopmentMode].
+  DevelopmentMode() : super(appName: 'App title', endpoint: 'https://api.dev.com', color: Colors.blue);
+}
+
+/// Production mode configuration.
+class ProductionMode extends AppConfig {
+  /// Creates a new instance of [ProductionMode].
+  ProductionMode() : super(appName: 'App title', endpoint: 'https://api.prod.com', color: Colors.green);
+}
